312df298076e9f0369d07fff1323fe52a4c81f28
Joao <jaosantosp69@gmail.com>
2025-07-12 19:26:20 -0300

    Inserindo os dados da conta via terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -3,6 +3,18 @@ import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner teclado = new Scanner(System.in);
-        System.out.println("Por gentileza, insira o número da agência!!");
+
+        double saldo = 100.00;
+
+        System.out.println("Digite o nome cadastrado na conta conta: ");
+        String nomeCliente = teclado.next();
+
+        System.out.println("Por gentileza, insira o número da agência: ");
+        int agencia = teclado.nextInt();
+
+        System.out.println("No proximo passo, digite o numero da conta, 4 digitos apenas: ");
+        int conta = teclado.nextInt();
+
+
     }
 }

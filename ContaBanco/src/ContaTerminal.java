eb40948a926c956657b3dd8052571df9808189cc
Joao <jaosantosp69@gmail.com>
2025-07-12 19:14:11 -0300

    Implementando a classe Scanner

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Por gentileza, insira o núnemo da agência!");
+    }
+}
